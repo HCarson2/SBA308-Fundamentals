9e3f7c3481dfd466615e1dc7aab15cad8f2fd130
HaroldCarson <mair0610@gmail.com>
2025-01-12 22:34:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Course info data  type
+const courseInfo = { id: 1, name: "SBA JavaScript Fundamentals"};
+
+//Assignment Group data type
+const assignmentGroups = [{
+    id: 1,
+    name: "Homework",
+    course_id: 1,
+    group_weight: 50,
+    assignments: [
+        {
+            id: 101,
+            name: "Assignment 1",
+            due_at: "2025-01-12",
+            points_possible: 100,
+
+
+    },
+{
+    id: 102,
+    name: "Assignment 2",
+    due_at: "2025-01-12",
+    points_possible: 200,
+}]
+}]
