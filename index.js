e863ed6112ceb8c9e268e4e4daf6e2b5f0f79923
HaroldCarson <mair0610@gmail.com>
2025-01-12 23:32:17 -0500

    at console.log

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
+
 //Course info data  type
-const courseInfo = { id: 1, name: "SBA JavaScript Fundamentals"};
+const courseInfo = { id: 1, name: "SBA JavaScript Fundamentals" };
 
 //Assignment Group data type
-const assignmentGroups = [{
+const assignmentGroups = [
+  {
     id: 1,
     name: "Homework",
     course_id: 1,
     group_weight: 50,
     assignments: [
-        {
-            id: 101,
-            name: "Assignment 1",
-            due_at: "2025-01-12",
-            points_possible: 100,
-
-
+      {
+        id: 101,
+        name: "Assignment 1",
+        due_at: "2025-01-12",
+        points_possible: 100,
+      },
+      {
+        id: 102,
+        name: "Assignment 2",
+        due_at: "2025-01-12",
+        points_possible: 200,
+      },
+    ],
+  },
+];
+//Learner Submmission data type
+const learnerSubmissions = [
+  {
+    learner_id: 1,
+    assignment_id: 101,
+    submission: {
+      submitted_at: "2025-01-12",
+      score: 80,
     },
-{
-    id: 102,
-    name: "Assignment 2",
-    due_at: "2025-01-12",
-    points_possible: 200,
-}]
-}]
+  },
+  {
+    learner_id: 1,
+    assignment_id: 102,
+    submission: {
+      submitted_at: "2025-01-12",
+      score: 190,
+    },
+  }
+];
+
+console.log(getLearnerData(courseInfo, assignmentGroups, learnerSubmissions));
